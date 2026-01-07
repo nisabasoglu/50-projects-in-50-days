1a26b1ec83526f390560345883f55e9a2f1b6496
Racihe Nisa Başoğlu <nisabasoglu67@gmail.com>
2026-01-07 17:47:39 +0300

    added animated navigation

---
+++ Day18-animatedNavigation/script.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active')
+})
+
+
+
+/* 
+
+add() → class ekle
+remove() → class sil
+toggle() → varsa sil, yoksa ekle
+
+*/
