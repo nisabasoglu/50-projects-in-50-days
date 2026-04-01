fd09e61248d0447d11ad18065af10425c7e246ee
Racihe Nisa Başoğlu <nisabasoglu67@gmail.com>
2026-04-01 15:33:44 +0300

    added imageCrousel project

---
+++ Day39-imageCrousel/script.js
@@ -0,0 +1,40 @@
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+const img = document.querySelectorAll('#imgs img') //Sadece #imgs id'li elementin içindeki img'leri seçer
+
+let idx = 0
+
+let interval = setInterval(run, 2000)
+
+function run () {
+    idx++
+    changeImage()
+}
+
+function changeImage () {
+    if(idx > img.length - 1) {
+        idx = 0
+    }else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translate(${-idx * 500}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
+})
+
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
